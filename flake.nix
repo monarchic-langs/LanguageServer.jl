b76b5c78a1188694352e72154611e100b63f9f33
midischwarz12 <midischwarz@proton.me>
2026-07-29 21:05:12 -0500

    ci: add flake checks

--- flake.nix
+++ flake.nix
@@ -3,7 +3,11 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = {nixpkgs, ...}: let
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
     julia = pkgs.julia.withPackages ["LanguageServer"];
@@ -19,6 +23,27 @@
     packages.${system} = {
       default = julia-language-server;
       inherit julia-language-server;
+    };
+    checks = {
+      ${system} = {
+        inherit (self.packages.${system}) default;
+
+        flake-format =
+          pkgs.runCommand "julia-language-server-flake-format-check"
+          {nativeBuildInputs = [pkgs.alejandra];}
+          ''
+            alejandra --check ${./flake.nix}
+            touch $out
+          '';
+
+        language-server-smoke =
+          pkgs.runCommand "julia-language-server-smoke"
+          {nativeBuildInputs = [julia];}
+          ''
+            julia -e 'using LanguageServer; println("LanguageServer loaded")'
+            touch $out
+          '';
+      };
     };
     devShells.${system}.default = pkgs.mkShell {
       packages = [julia julia-language-server];
